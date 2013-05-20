5a5a7f6f33ed9ac4c0f016bfb209b88dd86ab812
kazuyukitanimura <kazuyukitanimura@gmail.com>
2013-05-19 17:47:11 -0700

    bubble history

--- public/javascripts/edit.js
+++ public/javascripts/edit.js
@@ -1,6 +1,7 @@
 $(function() {
   var rand = function(max, min) {
-    return Math.floor(Math.random() * (max - min + 1)) + (min? min: 0);
+    min = min || 0;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   };
   var $allBubble = $('#all-bubble');
   var $img = $('img#target');
@@ -12,8 +13,11 @@ $(function() {
     var $input = $this.find('#bubble-input');
     var text = $input.val();
     var rmsec = parseFloat($this.find('#rmsec').val()) * 1000;
-    var x = rand(90, 10);
-    var y = rand(90, 10);
+    if (!text || rmsec <= 0) {
+      return false;
+    }
+    var x = rand(90);
+    var y = rand(90);
     var $bubble = $(['<div class="bubble animated bounce" style="top:', y, '%; left:', x, '%;">', text, '</div>'].join(''));
     var duration = parseInt($img.data('duration'), 10);
     var elapsed = (e.timeStamp - startTime) % duration;
@@ -25,15 +29,27 @@ $(function() {
       rmsec: rmsec
     };
     $allBubble.append($([
-      '<li><a>',
-        JSON.stringify(data),
-      '</a></li>'
+      '<div class="form-inline bubble-entry">',
+        '<button class="close pull-right">&times;</button>',
+        '<div class="input-prepend">',
+          '<span class="add-on">Time</span>',
+          '<input class="input-mini" type="number" value="', data.elapsed / 1000.0,'">',
+        '</div>',
+        '<div class="input-prepend">',
+          '<span class="add-on">Hide after</span>',
+          '<input class="input-mini" type="number" value="', data.rmsec / 1000.0,'">',
+        '</div>',
+        '<div class="input-prepend">',
+          '<span class="add-on">Text</span>',
+          '<input class="input-mini" type="text" value="', data.text,'">',
+        '</div>',
+      '</div>'
     ].join('')));
     setTimeout($bubble.hide.bind($bubble, 0, function(duration) {
       this.data('hideTimer', setInterval(this.hide.bind(this), duration));
     }.bind($bubble, duration)), rmsec);
     $bubble.data('showTimer', setInterval($bubble.show.bind($bubble), duration));
-    $this.after($bubble);
+    $img.after($bubble);
     $input.val('').focus();
     return false;
   });
