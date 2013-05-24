09986720aee4082f3750fb95bd7d09b857bac2e0
kazuyukitanimura <kazuyukitanimura@gmail.com>
2013-05-23 23:34:07 -0700

    query img

--- routes/edit.js
+++ routes/edit.js
@@ -3,14 +3,21 @@
  */
 
 exports.index = function(req, res) {
-  res.render('edit', {
-    title: 'UasApp',
-    description: 'Experimental short video sharing service',
-    img: {
-      title: 'neko',
-      url: '/images/f8f7a03a.gif',
-      duration: '4200'
-    }
-  });
+  var img = req.query.img;
+  console.log(img);
+  var durationMatch = /\w+-\d+-\d+-\d+-(\d+)\.\w+/.exec(img); // digest_height_width_size_duration.gif
+  if (durationMatch) {
+    res.render('edit', {
+      title: 'UasApp',
+      description: 'Experimental short video sharing service',
+      img: {
+        title: 'neko',
+        url: '/images/' + img,
+        duration: durationMatch[1]
+      }
+    });
+  } else {
+    res.send(404);
+  }
 };
 
