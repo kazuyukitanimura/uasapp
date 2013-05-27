ebd9675627fd787d8b4f029c64a7e794daed337c
kazuyukitanimura <kazuyukitanimura@gmail.com>
2013-05-27 03:45:06 -0700

    non stopping on error

--- app.js
+++ app.js
@@ -45,3 +45,7 @@ http.createServer(app).listen(app.get('port'), function() {
   console.log("Express server listening on port " + app.get('port'));
 });
 
+// Do no stop running
+process.on('uncaughtException', function (err) {
+  console.error(err);
+});
