ece5fc4a9d5fe886656baa87e734ab24b7d09029
kazuyukitanimura <kazuyukitanimura@gmail.com>
2013-06-09 16:45:34 -0700

    get timeline

--- public/javascripts/timeline.js
+++ public/javascripts/timeline.js
@@ -1,3 +1,9 @@
 $(function() {
-
+  var apiBaseUrl = '/api/v1/';
+  $.get(apiBaseUrl + 'timeline').done(function(data) {
+    console.log(data);
+  }).fail(function() {
+    $('#error-message').show();
+  });
 });
+
