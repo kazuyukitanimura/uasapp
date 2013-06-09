506d056766b94ce9a90e743e5268d1d62f575616
kazuyukitanimura <kazuyukitanimura@gmail.com>
2013-06-09 01:58:17 -0700

    pass arguments

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,7 @@
 var db = require('riak-js').getClient();
 var config = require('../config');
 var bucket = config.bubbleBucket;
+var timeRadix = config.timeRadix;
 
 var getDuration = exports.getDuration = function(img) {
   var durationMatch = /\w+-\d+-\d+-\d+-(\d+)\.\w+/.exec(img); // digest_height_width_size_duration.gif
@@ -22,29 +23,28 @@ exports.index = function(req, res) {
         data: Riak.mapValuesJson(value)[0]
       }];
     }).reduce(function(valueList, arg) {
+      var timeRadix = arg.timeRadix;
       return valueList.sort(function(a, b) {
         var aTimeSequenceMachine = a.id.split('-');
         var bTimeSequenceMachine = b.id.split('-');
-        if (aTimeSequenceMachine.length === 3 && bTimeSequenceMachine.length === 3) {
-          var aTime = parseInt(aTimeSequenceMachine[0], 36);
-          var bTime = parseInt(bTimeSequenceMachine[0], 36);
-          var aSequence = parseInt(aTimeSequenceMachine[1], 36);
-          var bSequence = parseInt(bTimeSequenceMachine[1], 36);
-          if (aTime > bTime) {
-            return - 1;
-          } else if (aTime < bTime) {
-            return 1;
-          } else if (aSequence > bSequence) {
-            return - 1;
-          } else if (aSequence < bSequence) {
-            return 1;
-          }
-        }
-        return 0;
-      });
-    }).reduce('Riak.reduceSlice', [start, end]).run(function(err, data) {
+        var aTime = parseInt(aTimeSequenceMachine[0], timeRadix);
+        var bTime = parseInt(bTimeSequenceMachine[0], timeRadix);
+        var aSequence = parseInt(aTimeSequenceMachine[1], timeRadix);
+        var bSequence = parseInt(bTimeSequenceMachine[1], timeRadix);
+        return (bTime - aTime) || (bSequence - aSequence);
+      }).slice(arg.start, arg.end);
+    },
+    {
+      timeRadix: timeRadix,
+      start: start,
+      end: end
+    }).run(function(err, data) {
       console.log(data);
-      res.send(data);
+      if (Array.isArray(data)) { // tentative error check
+        res.send(data);
+      } else {
+        res.send(500);
+      }
     });
   } else {
     res.send(404);
