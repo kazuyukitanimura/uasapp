f72353d7fb726b09616836b616df258403e13c64
kazuyukitanimura <kazuyukitanimura@gmail.com>
2013-06-09 23:48:18 -0700

    image tag

--- public/javascripts/timeline.js
+++ public/javascripts/timeline.js
@@ -4,9 +4,12 @@ $(function() {
     var $timelineContainer = $('#timeline');
     for (var i = 0, l = entries.length; i < l; i++) {
       var entry = entries[i];
+      var data = entry.data;
       var $entry = $('<div></div>', {'id': entry.id}).appendTo($timelineContainer);
       var $imgTitle = $('<h2></h2>', {'class': 'img-title'}).appendTo($entry);
-      var $title = $('<span></span>', {'class': 'title'}).text(entry.data.title).appendTo($imgTitle);
+      var $title = $('<span></span>', {'class': 'title'}).text(data.title).appendTo($imgTitle);
+      var $imgWrapper = $('<div></div>', {'class': 'img-wrapper'}).appendTo($entry);
+      var $img = $('<img>').data('imgurl', data.url).data('duration', data.duration).appendTo($imgWrapper);
     }
     console.log(entries);
   }).fail(function() {
