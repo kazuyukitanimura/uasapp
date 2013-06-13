dbeab87539ad85c62df904517aa6666fbc5cf03c
kazuyukitanimura <kazuyukitanimura@gmail.com>
2013-06-13 01:30:57 -0700

    thumbnails

--- public/javascripts/timeline.js
+++ public/javascripts/timeline.js
@@ -9,28 +9,37 @@ $(function() {
     for (var i = 0, l = entries.length; i < l; i++) {
       var entry = entries[i];
       var data = entry.data;
-      var $entry = $('<div></div>', {
+      var $entry = $('<li></li>', {
         'id': entry.id,
-        'class': 'row'
+        'class': 'span12 clearfix'
       }).appendTo($timelineContainer);
-      var $imgWrapper = $('<div></div>', {
-        'class': 'img-wrapper span6'
+      var $entryInner = $('<div></div>', {
+        'class': 'thumbnail clearfix'
       }).appendTo($entry);
+      var $imgWrapper = $('<div></div>', {
+        'class': 'img-wrapper pull-left span6 clearfix'
+      }).appendTo($entryInner);
       var $img = $('<img>', {
         'class': 'img-tl'
       }).attr('src', data.url).data('duration', data.duration).appendTo($imgWrapper);
       var $ctrlWrapper = $('<div></div>', {
-        'class': 'ctrl-wrapper span6'
-      }).appendTo($entry);
-      var $imgTitle = $('<h2></h2>', {
+        'class': 'ctrl-wrapper pull-left caption'
+      }).appendTo($entryInner);
+      var $imgTitle = $('<h3></h3>', {
         'class': 'img-title'
       }).appendTo($ctrlWrapper);
       var $title = $('<span></span>', {
         'class': 'title'
       }).text(data.title).appendTo($imgTitle);
       var $editBtn = $('<button></button>', {
-        'class': 'btn'
-      }).text('Edit').appendTo($ctrlWrapper);
+        'class': 'btn btn-primary'
+      }).text('Add Bubbles').appendTo($ctrlWrapper);
+      var $renewBtn = $('<button></button>', {
+        'class': 'btn btn-info'
+      }).text('Renew').appendTo($ctrlWrapper);
+      var $delBtn = $('<button></button>', {
+        'class': 'btn btn-danger'
+      }).text('Delete').appendTo($ctrlWrapper);
     }
     console.log(entries);
   }).fail(function() {
