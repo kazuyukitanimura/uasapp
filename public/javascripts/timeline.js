ab91cb8adb43540a61f3a8261c515bec571af2da
kazuyukitanimura <kazuyukitanimura@gmail.com>
2013-06-12 23:00:32 -0700

    meta

--- public/javascripts/timeline.js
+++ public/javascripts/timeline.js
@@ -6,11 +6,11 @@ $(function() {
       var entry = entries[i];
       var data = entry.data;
       var $entry = $('<div></div>', {'id': entry.id, 'class': 'row'}).appendTo($timelineContainer);
-      var $imgTitle = $('<h2></h2>', {'class': 'img-title'}).appendTo($entry);
-      var $title = $('<span></span>', {'class': 'title'}).text(data.title).appendTo($imgTitle);
       var $imgWrapper = $('<div></div>', {'class': 'img-wrapper span6'}).appendTo($entry);
       var $img = $('<img>', {'class': 'img-tl'}).attr('src', data.url).data('duration', data.duration).appendTo($imgWrapper);
       var $ctrlWrapper = $('<div></div>', {'class': 'ctrl-wrapper span6'}).appendTo($entry);
+      var $imgTitle = $('<h2></h2>', {'class': 'img-title'}).appendTo($ctrlWrapper);
+      var $title = $('<span></span>', {'class': 'title'}).text(data.title).appendTo($imgTitle);
       var $editBtn = $('<button></button>', {'class': 'btn'}).text('Edit').appendTo($ctrlWrapper);
     }
     console.log(entries);
